789d3757257051f03fcb65e56d38c4a23b97c2b8
theTazBaz <150133311+theTazBaz@users.noreply.github.com>
2024-12-26 20:13:16 +0530

    start screen
    start screen for when the game is loaded.

---
+++ pixelmon/dist/scene2.js
@@ -0,0 +1,12 @@
+import Phaser from "phaser";
+export default class scene2 extends Phaser.Scene {
+    constructor() {
+        super("scene2");
+    }
+    create() {
+        this.add.text(480, 270, "Welcome to Scene 2", {
+            font: "48px Arial",
+            color: "#ffffff",
+        }).setOrigin(0.5);
+    }
+}
